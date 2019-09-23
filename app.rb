3d6b1bf4236bf4c4d4236ea0df405173f2323d08
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-23 12:00:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   post '/reverse' do
   
   orginal_string = params["string"]
-  reversed_string = orginal_string.reverse 
+  @reversed_string = orginal_string.reverse 
     
   erb :reversed
   end
